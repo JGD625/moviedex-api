939408d0e11862bb42137981bcdf028ea8cabe0b
Julie <39500350+JGD625@users.noreply.github.com>
2019-05-31 09:30:28 -0400

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
 const helmet = require('helmet')
-const MOVIEDEX = require('./moviedex-small.json')
+const MOVIES = require('./moviedex-small.json')
 
 const app = express()
 
